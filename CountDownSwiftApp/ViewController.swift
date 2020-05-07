458ae80c29a6a8956b9153e77fb2cbadb9f98e2c
richard <richardp.leandro@gmail.com>
2020-05-07 20:14:18 -0300

    Initial Commit

---
+++ CountDownSwiftApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountDownSwiftApp
+//
+//  Created by Richard Leandro on 07/05/20.
+//  Copyright © 2020 Richard Leandro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
