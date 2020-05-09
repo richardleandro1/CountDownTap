e53446c230fd09504a2896e94e87b95c4fec1192
richard <richardp.leandro@gmail.com>
2020-05-09 14:36:10 -0300

    First commit

--- CountDownSwiftApp/ViewController.swift
+++ CountDownSwiftApp/ViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var TextLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self,
+                                                         action: #selector(handleTapAnimations)))
+    }
+    
+    @objc fileprivate func handleTapAnimations(){
+        UIView.animate(withDuration: 0.5, delay: 0,
+                       usingSpringWithDamping: 1, initialSpringVelocity: 1,
+                       options: .curveEaseOut, animations: {
+                        
+                        self.TextLabel.alpha = 0
+                        self.TextLabel.transform = self.TextLabel.transform.translatedBy(x: 0, y: -150)
+                        
+        })
     }
 
 
